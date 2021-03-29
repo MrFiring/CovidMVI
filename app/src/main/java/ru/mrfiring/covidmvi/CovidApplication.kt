c052a766896c511163cbc7ae030e424c57821b65
Kirill Bondarev <Gut54566@yandex.ru>
2021-03-29 20:37:07 +0700

    Initial commit

---
+++ app/src/main/java/ru/mrfiring/covidmvi/CovidApplication.kt
@@ -0,0 +1,5 @@
+package ru.mrfiring.covidmvi
+
+import android.app.Application
+
+class CovidApplication: Application()
---
+++ app/src/main/java/ru/mrfiring/covidmvi/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package ru.mrfiring.covidmvi.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import ru.mrfiring.covidmvi.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
