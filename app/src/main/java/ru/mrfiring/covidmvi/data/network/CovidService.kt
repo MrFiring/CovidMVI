e8632c3107d3ec0d8f0c1e3894f7b0200f00d107
Kirill Bondarev <Gut54566@yandex.ru>
2021-04-02 23:29:08 +0700

    Added methods to the service to retrieve stats of continents and historical data. DTO also impl.

--- app/src/main/java/ru/mrfiring/covidmvi/data/network/CovidService.kt
+++ app/src/main/java/ru/mrfiring/covidmvi/data/network/CovidService.kt
@@ -2,16 +2,44 @@ package ru.mrfiring.covidmvi.data.network
 
 import io.reactivex.Single
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 const val BASE_URL = "https://disease.sh/v3/"
 
+enum class SortType(val str: String){
+    CASES("cases"),
+    TODAY_CASES("todayCases"),
+    DEATHS("deaths"),
+    TODAY_DEATHS("todayDeaths"),
+    RECOVERED("recovered"),
+    ACTIVE("active"),
+    CRITICAL("critical"),
+    CASES_PER_ONE_MILLION("casesPerOneMillion"),
+    DEATHS_PER_ONE_MILLION("deathsPerOneMillion")
+}
+
 interface CovidService {
-    @GET("/v3/covid-19/all")
+    @GET("covid-19/all")
     fun getGlobalStats(
         @Query("yesterday") showYesterday: Boolean = false,
         @Query("twoDaysAgo") showTwoDaysAgo: Boolean = false,
         @Query("allowNull") allowNullValues: Boolean = false
     ): Single<GlobalStats>
+
+
+    @GET("covid-19/continents")
+    fun getStatsByContinents(
+        @Query("sort") sortBy: SortType = SortType.CASES,
+        @Query("yesterday") showYesterday: Boolean = false,
+        @Query("twoDaysAgo") showTwoDaysAgo: Boolean = false,
+        @Query("allowNull") allowNullValues: Boolean = false
+    ): Single<List<ContinentStats>>
+
+    @GET("covid-19/historical/{country}")
+    fun getHistoricalStatsByCountryName(
+        @Path("country", encoded = true) countryName: String,
+        @Query("lastDays") lastDays: Int = 30
+    ): Single<CountryHistoricalStats>
 
 }
--- app/src/main/java/ru/mrfiring/covidmvi/data/network/DataTransferObjects.kt
+++ app/src/main/java/ru/mrfiring/covidmvi/data/network/DataTransferObjects.kt
@@ -24,3 +24,45 @@ data class GlobalStats(
     val affectedCountries: Int,
     @Json(name = "updated") val lastUpdate: Long
 )
+@JsonClass(generateAdapter = true)
+data class ContinentCoords(
+    val lat: Double,
+    val long: Double
+)
+
+@JsonClass(generateAdapter = true)
+data class ContinentStats(
+    val cases: Long,
+    val todayCases: Long,
+    val deaths: Long,
+    val todayDeaths: Long,
+    val recovered: Long,
+    val todayRecovered: Long,
+    val active: Long,
+    val critical: Long,
+    val casesPerOneMillion: Double,
+    val deathsPerOneMillion: Double,
+    val tests: Long,
+    val testsPerOneMillion: Double,
+    val population: Long,
+    @Json(name="continent") val continentName: String,
+    val activePerOneMillion: Double,
+    val recoveredPerOneMillion: Double,
+    val criticalPerOneMillion: Double,
+    @Json(name = "continentInfo") val continentCoords: ContinentCoords,
+    @Json(name = "updated") val lastUpdate: Long,
+    @Json(name = "countries") val contriesList: List<String>
+)
+
+@JsonClass(generateAdapter = true)
+data class HistoricalData(
+    val cases: Map<String, Long>,
+    val deaths: Map<String, Long>,
+    val recovered: Map<String, Long>
+)
+
+@JsonClass(generateAdapter = true)
+data class CountryHistoricalStats(
+    @Json(name = "country") val countryName: String,
+    @Json(name = "timeline") val data: HistoricalData
+)
