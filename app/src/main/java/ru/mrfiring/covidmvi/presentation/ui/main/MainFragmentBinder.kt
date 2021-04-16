8819b9d42c993f6f6a1c2f159f71a1786cd99674
Kirill Bondarev <Gut54566@yandex.ru>
2021-04-16 15:47:08 +0700

    Added combineLatest rxExtension to use in a complex screen. ViewModelTransformer.kt and MainFragmentBinder.kt refactored to combineLatest.

--- app/src/main/java/ru/mrfiring/covidmvi/presentation/ui/main/MainFragmentBinder.kt
+++ app/src/main/java/ru/mrfiring/covidmvi/presentation/ui/main/MainFragmentBinder.kt
@@ -6,19 +6,23 @@ import com.badoo.mvicore.android.AndroidBindings
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedFactory
 import dagger.assisted.AssistedInject
-import ru.mrfiring.covidmvi.presentation.features.GlobalStatsCacheFeature
+import ru.mrfiring.covidmvi.presentation.features.ContinentStatsFeature
 import ru.mrfiring.covidmvi.presentation.features.GlobalStatsFeature
-import ru.mrfiring.covidmvi.presentation.viewmodel.ViewModelTransformer1
-import ru.mrfiring.covidmvi.presentation.viewmodel.ViewModelTransformer2
+import ru.mrfiring.covidmvi.presentation.viewmodel.ViewModelPairedTransformer
+import ru.mrfiring.covidmvi.utils.combineLatest
 
 class MainFragmentBinder @AssistedInject constructor(
     @Assisted private val lifecycle: LifecycleOwner,
-    private val globalStatsCacheFeature: GlobalStatsCacheFeature,
+    private val continentStatsFeature: ContinentStatsFeature,
     private val globalStatsFeature: GlobalStatsFeature
-): AndroidBindings<MainFragment>(lifecycle) {
+) : AndroidBindings<MainFragment>(lifecycle) {
     override fun setup(view: MainFragment) {
-        binder.bind(globalStatsCacheFeature to view using ViewModelTransformer2())
-        binder.bind( globalStatsFeature to view using ViewModelTransformer1())
+        binder.bind(
+            combineLatest(
+                continentStatsFeature,
+                globalStatsFeature
+            ) to view using ViewModelPairedTransformer()
+        )
     }
 
     @AssistedFactory
--- app/src/main/java/ru/mrfiring/covidmvi/presentation/viewmodel/ViewModelTransformer.kt
+++ app/src/main/java/ru/mrfiring/covidmvi/presentation/viewmodel/ViewModelTransformer.kt
@@ -1,29 +1,21 @@
 package ru.mrfiring.covidmvi.presentation.viewmodel
 
-import ru.mrfiring.covidmvi.domain.DomainGlobalStats
-import ru.mrfiring.covidmvi.presentation.features.GlobalStatsCacheFeature
+import ru.mrfiring.covidmvi.presentation.features.ContinentStatsFeature
 import ru.mrfiring.covidmvi.presentation.features.GlobalStatsFeature
 
-class ViewModelTransformer1: (GlobalStatsFeature.State) -> ViewModel {
-    override fun invoke(state: GlobalStatsFeature.State): ViewModel = ViewModel(
-        globalStats = state.globalStats ?: DomainGlobalStats(
-            0, 0, 0 ,0 ,
-        0, 0, 0, 0,
-            0,0.0,0.0,0,
-            0.0,0,0.0,
-            0.0,0.0,0),
-        isLoading = state.isLoading
-    )
-}
+class ViewModelPairedTransformer: (
+    Pair<ContinentStatsFeature.State, GlobalStatsFeature.State>
+) -> ViewModel {
+    override fun invoke(
+        pair: Pair<ContinentStatsFeature.State, GlobalStatsFeature.State>
+    ): ViewModel {
+        val (continentState, globalState) = pair
 
-class ViewModelTransformer2: (GlobalStatsCacheFeature.State) -> ViewModel {
-    override fun invoke(state: GlobalStatsCacheFeature.State): ViewModel = ViewModel(
-        globalStats = state.globalStats ?: DomainGlobalStats(
-            0, 0, 0 ,0 ,
-            0, 0, 0, 0,
-            0,0.0,0.0,0,
-            0.0,0,0.0,
-            0.0,0.0,0),
-        isLoading = state.isLoading
-    )
+        return ViewModel(
+            globalState.globalStats,
+            continentState.continentStats,
+            continentState.isLoading || globalState.isLoading
+        )
+
+    }
 }
