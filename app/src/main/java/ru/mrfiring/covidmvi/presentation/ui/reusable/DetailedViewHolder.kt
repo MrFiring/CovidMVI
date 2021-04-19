0124361a0a9299d70b1163da543395f8555a9371
Kirill Bondarev <Gut54566@yandex.ru>
2021-04-19 21:58:14 +0700

    Added layout for detailed screens and template of detailed viewholder

---
+++ app/src/main/java/ru/mrfiring/covidmvi/presentation/ui/reusable/DetailedViewHolder.kt
@@ -0,0 +1,15 @@
+package ru.mrfiring.covidmvi.presentation.ui.reusable
+
+class DetailedViewHolder {
+
+    fun bind(){}
+
+    companion object{
+
+        fun from(): DetailedViewHolder {
+
+        }
+
+    }
+
+}
