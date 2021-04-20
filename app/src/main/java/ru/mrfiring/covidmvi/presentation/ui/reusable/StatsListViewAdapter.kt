077d55f75e9ccfff288ce68daa32dbb44cab0bf1
Kirill Bondarev <Gut54566@yandex.ru>
2021-04-20 17:06:49 +0700

    StatsListViewAdapter.kt impl

---
+++ app/src/main/java/ru/mrfiring/covidmvi/presentation/ui/reusable/StatsListViewAdapter.kt
@@ -0,0 +1,47 @@
+package ru.mrfiring.covidmvi.presentation.ui.reusable
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+
+private const val VIEW_TYPE_SIMPLE = 0
+private const val VIEW_TYPE_DETAILED = 1
+
+
+class StatsListViewAdapter(
+    private val items: List<StatsListDataItem>
+) : BaseAdapter() {
+
+    override fun getCount(): Int = items.size
+
+    override fun getItem(position: Int): StatsListDataItem = items[position]
+
+    override fun getItemId(position: Int): Long = position.toLong()
+
+    override fun getItemViewType(position: Int): Int = when (getItem(position)) {
+        is StatsListDataItem.Simple -> VIEW_TYPE_SIMPLE
+
+        is StatsListDataItem.Detailed -> VIEW_TYPE_DETAILED
+    }
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View =
+        when (getItemViewType(position)) {
+            VIEW_TYPE_SIMPLE -> {
+                val holder = SimpleStatsViewHolder.from(requireNotNull(parent))
+                holder.bind(getItem(position) as StatsListDataItem.Simple)
+                holder.root
+            }
+
+            VIEW_TYPE_DETAILED -> {
+                val holder = DetailedStatsViewHolder.from(requireNotNull(parent))
+                holder.bind(getItem(position) as StatsListDataItem.Detailed)
+                holder.root
+            }
+
+            else -> {
+                throw TypeCastException("Unknown viewType at position: $position")
+            }
+
+        }
+
+}
