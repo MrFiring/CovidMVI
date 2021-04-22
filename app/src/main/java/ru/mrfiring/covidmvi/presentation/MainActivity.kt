cd819dd8073fed266f2544c71e268bfc61665111
Kirill Bondarev <Gut54566@yandex.ru>
2021-04-23 01:44:58 +0700

    ActivityMain don't use viewbinding anymore. Created new DI module that is responsible for providing NavController

--- app/src/main/java/ru/mrfiring/covidmvi/presentation/MainActivity.kt
+++ app/src/main/java/ru/mrfiring/covidmvi/presentation/MainActivity.kt
@@ -3,18 +3,14 @@ package ru.mrfiring.covidmvi.presentation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import dagger.hilt.android.AndroidEntryPoint
-import ru.mrfiring.covidmvi.databinding.ActivityMainBinding
+import ru.mrfiring.covidmvi.R
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
+        setContentView(R.layout.activity_main)
     }
 
 }
