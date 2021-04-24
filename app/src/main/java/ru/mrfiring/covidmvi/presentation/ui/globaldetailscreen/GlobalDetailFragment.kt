ead661529cb4ca994d23a7640bfc5ae30da863fb
Kirill Bondarev <Gut54566@yandex.ru>
2021-04-24 19:20:17 +0700

    Removed unused imports

--- app/src/main/java/ru/mrfiring/covidmvi/presentation/ui/globaldetailscreen/GlobalDetailFragment.kt
+++ app/src/main/java/ru/mrfiring/covidmvi/presentation/ui/globaldetailscreen/GlobalDetailFragment.kt
@@ -6,15 +6,11 @@ import android.view.View
 import android.view.ViewGroup
 import dagger.hilt.android.AndroidEntryPoint
 import io.reactivex.Completable
-import io.reactivex.Scheduler
-import io.reactivex.Single
-import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.functions.Consumer
 import io.reactivex.schedulers.Schedulers
 import ru.mrfiring.covidmvi.databinding.FragmentGlobalBinding
 import ru.mrfiring.covidmvi.presentation.event.UiEvent
 import ru.mrfiring.covidmvi.presentation.ui.reusable.detailstats.StatsListViewAdapter
-import ru.mrfiring.covidmvi.presentation.ui.reusable.detailstats.asDataItemList
 import ru.mrfiring.covidmvi.presentation.util.ObservableSourceFragment
 import ru.mrfiring.covidmvi.presentation.viewmodel.ViewModel
 import javax.inject.Inject
